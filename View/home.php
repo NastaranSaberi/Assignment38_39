24b385eab66e2819eba4e9b0998aeef4eaeeb0eb
NastaranSaberi <94223830+NastaranSaberi@users.noreply.github.com>
2022-11-12 05:26:59 +0330

    Update home.php

--- View/home.php
+++ View/home.php
@@ -5,7 +5,6 @@
     include "View/header.php";
     include "View/navbar.php";
     
-
  
 
 $user = $db->query("SELECT * FROM users WHERE id = $user_id")->fetch_assoc();
@@ -124,14 +123,13 @@ $user = $db->query("SELECT * FROM users WHERE id = $user_id")->fetch_assoc();
                                 </p>
                                   
                                 
-                                <form  action="follow_unfollow_user" method="post">
-                                <button class="btn" type="button" style="margin-left:75px;border-radius: 15px;width:88px;height:31px;border: none; background-color:#2374E1;color:white;font-weight:500;padding: 1px;margin-top: -3pc;" onclick= 'follow_unfollow()' id="follow" >
+                                <form id="form-follow-<?php echo $user_id_follow; ?>">
+                                <button class="btn" type="button" style="margin-left:75px;border-radius: 15px;width:88px;height:31px;border: none; background-color:#2374E1;color:white;font-weight:500;padding: 1px;margin-top: -3pc;" onclick= 'follow_unfollow(<?php echo $user_id_follow; ?>)' id="follow-<?php echo $user_follow; ?>" >
                                 follow
                                    
                                 </button>
                                 
-                                <input type="hidden" name="followings_id" value="<?php echo $user_id; ?>">
-                   
+                                <input type="hidden" name="followers_id" value="<?php echo $user_id_follow; ?>">
                              
                                 </form>
 
@@ -154,24 +152,9 @@ $user = $db->query("SELECT * FROM users WHERE id = $user_id")->fetch_assoc();
                 </div>
                 
 
-
-
-
-
-
-
                 <div>
                     <img src="<?php echo $post["media"]; ?>" class="card-img-top "  alt="..."  loading="lazy" style="height: 431px;">
                 </div>
-
-
-
-
-
-
-
-
-
 
 
                 <div>
@@ -281,144 +264,6 @@ $user = $db->query("SELECT * FROM users WHERE id = $user_id")->fetch_assoc();
             </div>
         </div>
         <?php endforeach; ?>
-
- 
-<script>
-
-
-    function follow_unfollow(){
-        let follow = document.getElementById("follow");
-        follow.style.background = "white";
-        follow.style.color = "black";
-        follow.innerHTML = "Unfollow";
-        
-        
-    }
-
-
-   function send_like(post_id) {
-
-    let form = document.getElementById("form-like-" + post_id);
-    let form_data = new FormData(form);
-
-    let btn = document.getElementById("btn-likes-" + post_id);
-    let count_number_tag = document.getElementById("count-like-" + post_id);
-
-    fetch("send_like",{
-        method:"post",
-        body:form_data
-    }).then(
-        result => result.text()
-    ).then(result => {
-        if (result == 1) {
-            // color
-            btn.style.color = "#DC143C";
-            btn.classList.remove("far");
-            btn.classList.add("fas");
-
-            // number likes
-            let number = count_number_tag.innerHTML;
-            number++;
-            count_number_tag.innerHTML = number;
-        } else if (result == 0) {
-            // color
-            btn.style.color = "white";
-            btn.classList.remove("fas");
-            btn.classList.add("far");
-
-            // number of likes
-            let number = count_number_tag.innerHTML;
-            number--;
-            count_number_tag.innerHTML = number;
-        }
-    }).catch(error => {
-        console.log(error)
-    });
-
-}
-
-
-
-
-
-
-    //Fetch API instead of XMLHttpRequest for AJAX
-    function send_comment(post_id , username) {
-        
-        let form = document.getElementById("form-comment-" + post_id);
-        let form_data = new FormData(form);
-
-
-    fetch("send_comment",{
-        method:"post",
-        body:form_data
-    }).then(result => {
-    
-
-   
-        let list_comments = document.getElementById("list_comments_"+post_id);
-
-        let li = document.createElement("LI");
-        li.classList.add("list-group-item");
-        li.classList.add("list-group-item-action");
-        li.classList.add("mt-1");
-        li.classList.add("lisss");
-
-        // let img = document.createElement("IMG");
-        // img.classList.add("mt-1");
-        // img.innerHTML = img;
-
-        let p = document.createElement("P");
-        p.classList.add("mb-1");
-        p.classList.add("p_li");
-        p.innerHTML = form_data.get("text");
-
-        let h5 = document.createElement("H5");
-        h5.classList.add("mb-1");
-        h5.classList.add("h5-cm");
-        h5.innerHTML = username;
-   
-        let small = document.createElement("SMALL");
-        small.classList.add("text-end");
-        small.classList.add("small_lis");
-        small.innerHTML = "just now".fontsize(2);
-
-
-        // let small = document.createElementById("SMALL");
-        // small.classList.add("text-end");
-        // small.innerHTML = form_data.get("time");
-
-
-        // let div = document.createElementById("DIV");
-        // div.classList.add(" d-flex w-100 ");
-
-        // let img = document.createElementById("IMG");
-        // img.classList.add("mt-1");
-
-        // let h5 = document.createElementById("H5");
-        // h5.classList.add("mb-1");
-        // h5.innerHTML = form_data.get("username");
-
-        // h5.appendChild(div); 
-        // img.appendChild(div);  
-        // div.appendChild(li);  
-        // li.appendChild(small);
-        // li.appendChild(img);  
-        li.appendChild(h5); 
-        li.appendChild(small); 
-        li.appendChild(p);   
-        list_comments.appendChild(li);
-   
-    }).catch(error => {
-        alert(error);
-    })
-}
-
-   
-
-
-
-</script>
 
 
 
