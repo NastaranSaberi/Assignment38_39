576d964f88d2748aeff11818452dbc021eee1cb9
NastaranSaberi <94223830+NastaranSaberi@users.noreply.github.com>
2022-11-12 05:29:46 +0330

    Update home.php

--- Controller/home.php
+++ Controller/home.php
@@ -4,6 +4,14 @@
     include "Controller/functions.php";
 
 
+   
+
+    $user = $db->query("SELECT * FROM users WHERE id = $user_id_follow")->fetch_assoc();
+    $user_id_follow = $_SESSION["user_id_follow"];
+
+    $user_id = $_SESSION["user_id"];
+
+
     $posts = $db->query("SELECT *, users.id AS id_karbar, posts.id AS id_post FROM
                         users INNER JOIN posts
                         ON users.id = posts.user_id
@@ -22,15 +30,21 @@
 
         $post["likes"] = $db->query("SELECT COUNT(*) AS count FROM likes WHERE post_id = $post_id")->fetch_assoc();
 
+        $post["like_user"]  = $db->query("SELECT * FROM likes WHERE post_id = $post_id AND user_id = $user_id")->num_rows;
+
         $post["comments"] = $db->query("SELECT * FROM
                                         comments INNER JOIN users 
                                         ON comments.user_id = users.id
                                         WHERE post_id = $post_id
                                         ORDER BY time DESC");
 
+
+
         $posts_array[] = $post;
         
     }
+
+    
    
 
     require "View/home.php";
